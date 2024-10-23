4fbe7b146be0c3e7870c1cb93470dfc6005feb19
AE010 <armasemiliano3@gmail.com>
2024-10-22 22:55:12 -0600

    Primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from "react"
-import Calculadora from "./components/Calculadora"
+import React from "react";
+import Calculadora from "./components/Calculadora";
 
 const App =() =>{
   return (
     <div className="App">
       <Calculadora/>
       <p className="developer">
+        *Los resultados trigonometricos son en radianes</p>
+        <p className="developer">
         Diseñado por <span>Armas Trujillo Emiliano</span>
       </p>
     </div>
   )
 }
 
-export default App
+export default App;
